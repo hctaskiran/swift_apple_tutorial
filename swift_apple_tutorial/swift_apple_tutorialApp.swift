e3cc508efa9c1fff0c85e6c1d7490028935c5bd2
Haktan Can Taşkıran <haktancantaskiran@gmail.com>
2024-03-20 00:30:41 +0300

    Initial Commit

---
+++ swift_apple_tutorial/swift_apple_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_apple_tutorialApp.swift
+//  swift_apple_tutorial
+//
+//  Created by Haktan Can Taşkıran on 20.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct swift_apple_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
